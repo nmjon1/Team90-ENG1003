0cb03661f505f2701c0ace8551cc9bace061236e
tgnon1 <lenci.han@gmail.com>
2016-04-06 12:39:35 +1000

    Added files via upload

---
+++ button.js
@@ -0,0 +1,6 @@
+document.getElementById("restartButton").onclick = restartButtonClicked;
+function restartButtonClicked()
+{
+button.innerHTML = “Restart”;
+    output.AreaRef.innerHTML = " ";
+}    
