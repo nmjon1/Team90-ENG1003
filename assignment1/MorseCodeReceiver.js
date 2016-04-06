dca176ebf0f5908fd2155f0b53071d9b09d47617
Jarred Gallina <jarred.gallina@qxbranch.com>
2016-04-06 14:18:13 +1000

    made the goddamn app goddamn work

--- assignment1/MorseCodeReceiver.js
+++ assignment1/MorseCodeReceiver.js
@@ -26,12 +26,45 @@
 
 // ADD YOUR ADDITIONAL FUNCTIONS AND GLOBAL VARIABLES HERE
 var blueLength, redLength;
+var signal_history = [];
+var current_letter = '';
+var lookup=
+{AB:"a", BAAA: "b", BABA:"c", BAA:"d",A:"e", AABA:"f", BBA:"g", AAAA:"h", AA: "i", ABBB:"j", BAB: "k", ABAA:"l", BB:"m", BA:"n", BBB:"o", ABBA:"p", BBAB:"q", ABA:"r", AAA:"s", B:"t", AAB:"u", AAAB:"v", ABB:"w", BAAB:"x", BABB:"y", BBAA:"z", BBBB:"0", ABBBB:"1", AABBB:"2", AAABB:"3", AAAAB:"4", AAAAA:"5", BAAAA:"6", BBAAA:"7", BBBAA:"8", BBBBA:"9", BABBA:"(", BABBAB:")", ABAABA:"\"", BAAAB: "=", ABBBBA: "'", BAABA:'"', ABABA:"+", BBBAAA:":", ABABAB:".", BBAABB:",", AABBAA:"?", BAAAAB:"-", ABBABA:"@", AAABAAB:"$", AABBAB:"_", BABABB:"!", ABAB:"<br>", AAABAB:"End transmission"};
 function detectRedLength() {
-    
+
 }
 
 function detectBlueLength() {
-    
+
+}
+
+function compare_arrays(a,b) {
+    if (a === b) {return true}
+    if (a.length != b.length) {return false}
+
+    for (i = 0; i < a.length; i++) {
+        if (a[i] != b[i]) {
+            return false
+        }
+    }
+    return true
+}
+
+function check_history (signal){
+    signal_history.push(signal);
+    console.log(signal_history);
+    if (compare_arrays(signal_history.slice(-4),[true, true, true, false])) {
+        current_letter += 'B'
+    } else if (compare_arrays(signal_history.slice(-2),[true, false])) {
+        current_letter += 'A'
+    } else if (compare_arrays(signal_history.slice(-4),[false, false, false, false])) {
+        if (lookup[current_letter] == "End transmission") {
+            messageFinished()
+        } else if (lookup[current_letter] != undefined) {
+            document.getElementById('messageField').innerHTML += lookup[current_letter]
+        }
+        current_letter = ''
+    }
 }
 /*
  * This function is called once per unit of time with camera image data.
@@ -66,9 +99,11 @@ function decodeCameraImage(data) {
     //Numbers below to be tweeked when access to actual camera is obtained.
     if (meanData[0] <= 150 && meanData[1] <= 150 && meanData[2] >= 200) {
         console.log(false);
+        check_history(false);
         return false;
     } else if (meanData[0] >= 200 && meanData[1] <= 150 && meanData[2] <= 150) {
         console.log(true);
+        check_history(true);
         return true;
     } else {
         console.log("other");
