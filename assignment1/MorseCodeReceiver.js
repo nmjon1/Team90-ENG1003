cc20cfaa49402c1439e5010c3b197fde6d5f6b58
spher1 <spher1@student.monash.edu>
2016-04-09 14:48:13 +1000

    Cleaned up lookup object
    made the lookup object look nicer by not having it on a single line and added a comment explaining its purpose

--- assignment1/MorseCodeReceiver.js
+++ assignment1/MorseCodeReceiver.js
@@ -28,7 +28,16 @@
 var signal_history = []; // Stores the entire history of the signal as an array. An on signal is stored as true, and off as false.
 var current_element = ''; // Stores the dots and dashes in the current element. A signifies a dot, B signifies a dash
 var lookup=
-{AB:"a", BAAA: "b", BABA:"c", BAA:"d",A:"e", AABA:"f", BBA:"g", AAAA:"h", AA: "i", ABBB:"j", BAB: "k", ABAA:"l", BB:"m", BA:"n", BBB:"o", ABBA:"p", BBAB:"q", ABA:"r", AAA:"s", B:"t", AAB:"u", AAAB:"v", ABB:"w", BAAB:"x", BABB:"y", BBAA:"z", BBBB:"0", ABBBB:"1", AABBB:"2", AAABB:"3", AAAAB:"4", AAAAA:"5", BAAAA:"6", BBAAA:"7", BBBAA:"8", BBBBA:"9", BABBA:"(", BABBAB:")", ABAABA:'“', BAAAB: "=", ABBBBA: "'", BAABA:'/', ABABA:"+", BBBAAA:":", ABABAB:".", BBAABB:",", AABBAA:"?", BAAAAB:"-", ABBABA:"@", AAABAAB:"$", AABBAB:"_", BABABB:"!", ABAB:"<br>", AAABAB:"End transmission"};
+ +{AB:"a", BAAA: "b", BABA:"c", BAA:"d",A:"e", AABA:"f", 
+ +BBA:"g", AAAA:"h", AA: "i", ABBB:"j", BAB: "k", ABAA:"l", 
+ +BB:"m", BA:"n", BBB:"o", ABBA:"p", BBAB:"q", ABA:"r", AAA:"s", 
+ +B:"t", AAB:"u", AAAB:"v", ABB:"w", BAAB:"x", BABB:"y", BBAA:"z", 
+ +BBBB:"0", ABBBB:"1", AABBB:"2", AAABB:"3", AAAAB:"4", AAAAA:"5", 
+ +BAAAA:"6", BBAAA:"7", BBBAA:"8", BBBBA:"9", BABBA:"(", BABBAB:")", 
+ +ABAABA:'“', BAAAB: "=", ABBBBA: "'", BAABA:'/', ABABA:"+", BBBAAA:":", 
+ +ABABAB:".", BBAABB:",", AABBAA:"?", BAAAAB:"-", ABBABA:"@", AAABAAB:"$", 
+ +AABBAB:"_", BABABB:"!", ABAB:"<br>", AAABAB:"End transmission"};
+// This is the object which is used to convert dots (A) and dashes (A) into characters
 
 document.getElementById("restartButton").onclick = restartButtonClicked;
 function restartButtonClicked()
